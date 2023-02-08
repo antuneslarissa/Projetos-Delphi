66b9113e577187470a61f8a45bdceddeb5918e02
Larissa Antunes <105815306+antuneslarissa@users.noreply.github.com>
2023-02-08 07:31:19 -0300

    Add files via upload

---
+++ Calculadora.dpr
@@ -0,0 +1,14 @@
+program Calculadora;
+
+uses
+  Vcl.Forms,
+  uCalculadora in 'uCalculadora.pas' {frmPrincipal};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TfrmPrincipal, frmPrincipal);
+  Application.Run;
+end.
